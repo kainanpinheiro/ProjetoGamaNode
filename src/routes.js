c3de66a5fc36dfef8633e1f37f1d8562d70ac7fe
joaovillarb <36734606+joaovillarb@users.noreply.github.com>
2021-04-04 23:19:34 -0300

    remocao do locacaoitem-service, routes e handlers

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 const CadastroRoutes = require('./routes/cadastro-routes');
 const LivroRoutes = require('./routes/livro-routes');
 const LocacaoRoutes = require('./routes/locacao-routes');
-const LocacaoItemRoutes = require('./routes/locacaoitem-routes');
 const AuthRoutes = require('./security/auth-routes');
 
 module.exports = [
   ...LivroRoutes,
   ...LocacaoRoutes,
   ...CadastroRoutes,
-  ...LocacaoItemRoutes,
   ...AuthRoutes,
 ];
