6b4f9cccc59b46dc961af8911406aacb54fe29e4
joaovillarb <36734606+joaovillarb@users.noreply.github.com>
2021-04-05 18:31:57 -0300

    endpoint herokuapp

--- src/database/config/postgres.js
+++ src/database/config/postgres.js
@@ -5,6 +5,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
